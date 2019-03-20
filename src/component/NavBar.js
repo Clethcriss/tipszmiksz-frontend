26afb86c22f2808a2e0e0bfb1e50e119a1cf388d
Clethcriss <balazs.borsi.1986@gmail.com>
2019-03-20 14:59:50 +0100

    Foundation navbar implemented in the navbar component, with separeted situation logged in or not

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -7,9 +7,10 @@ export class NavBar extends React.Component {
         super(props);
         this.state = {
             isUserLoggedIn: false,
-            username: "";
+            username: ""
         };
         this.logOut = this.logOut.bind(this);
+        this.logIn = this.logIn.bind(this);
     }
 
     componentDidMount() {
@@ -23,14 +24,77 @@ export class NavBar extends React.Component {
 
     #functions
     logOut() {
-        this.setState({isUserLoggedIn: false});
+        this.setState({isUserLoggedIn: false, username: ""});
         window.location = "http://localhost:3000/";
     }
 
-    #render
+    logIn() {
+        this.setState({isUserLoggedIn: true, username: "Bazsi"});
+    }
 
+
+
+    #render
     render() {
-        return "";
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="dropdown menu" data-dropdown-menu>
+                        <li className="menu-text">TIPSZ MIKSZ</li>
+                        <li>
+                            <a href="#">Menu</a>
+                            <ul className="menu vertical">
+                                <li><a href="#">Bid</a></li>
+                                <li><a href="#">Result</a></li>
+                                <li><a href="#">About</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <LoggedInCase isLoggedIn = {this.state.isUserLoggedIn} username={this.state.username} logIn={this.logIn} logOut={this.logOut}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+function LogInButton(props) {
+    return (<button type="button" className="button" onClick={props.onClick}>Login</button>)
+}
+
+function LogOutButton(props) {
+    return (<button type="button" className="button" onClick={props.onClick}>Log Out</button>);
+}
+
+function GreetingsButton(props) {
+
+    return (<button type="button" className="button">Hello {props.username}!</button>)
+}
+
+function LoggedInCase(props) {
+    const isLoggedIn = props.isLoggedIn;
+
+    if (!isLoggedIn) {
+        return (<ul className="menu">
+            <li>
+                <button type="button" className="button">Register</button>
+            </li>
+            <li>
+                <LogInButton onClick={props.logIn}/>
+            </li>
+        </ul>);
+    } else {
+        return (
+            <ul className="menu">
+                <li>
+                    <GreetingsButton username={props.username}/>
+                </li>
+                <li>
+                    <LogOutButton onClick={props.logOut}/>
+                </li>
+            </ul>
+        );
     }
 }
 
